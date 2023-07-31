acee051dbf8a1f23d51d6f3841b7649233574bab
saicharanars <saicharan_@outlook.com>
2023-08-01 01:07:43 +0530

    Cart System

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,6 @@ const mongodb = require("mongodb");
 const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
-    
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
@@ -11,30 +10,26 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  console.log(req.user,"post add");
-  const product = new Product(
-    {
-        title:title,
-        price:price,
-        description:description,
-        imageUrl:imageUrl
-    }
-  );
-  product
-    .save()
-    .then((result) => {
-      // console.log(result);
-      console.log("Created Product");
-      res.redirect("/admin/products");
-    })
-    .catch((err) => {
-      console.log(err);
+exports.postAddProduct = async (req, res, next) => {
+  try {
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    console.log(req.user, "post add");
+    const product = new Product({
+      title: title,
+      price: price,
+      description: description,
+      imageUrl: imageUrl,
+      userId: req.user._id,
     });
+    const result = await product.save();
+    console.log("Created Product", result);
+    res.redirect("/admin/products");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -84,8 +79,11 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
+    // .select("title price -_id")
+    // .populate("userId", "name")
     .then((products) => {
+      console.log(products);
       res.render("admin/products", {
         prods: products,
         pageTitle: "Admin Products",
